7298c4f9426be41448fb60178794336de7bb4888
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-07-19 20:00:46 +0200

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closeBrackets = [];
+
+  bracketsConfig.forEach(function(pair) {
+    openBrackets.push(pair[0]);
+    closeBrackets.push(pair[1]);
+  });
+
+  let opened = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == closeBrackets[openBrackets.indexOf(opened[opened.length - 1])]) {
+      opened.pop();
+    } else if (openBrackets.includes(str[i])) {
+      opened.push(str[i]);
+    } else { 
+      return false;
+    };
+  }
+  return opened.length == 0;
 }
